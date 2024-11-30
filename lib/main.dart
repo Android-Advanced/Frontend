cdaedd607d9c0430ee7063655ac67311514b4000
yuseong95 <us95677@gmail.com>
2024-12-01 01:39:10 +0900

    [Firebase] Firebase 연동 설정 완료
    - build.gradle 파일에 minSdkVersion 23 및 multiDexEnabled 추가
    - Firebase Auth, Firestore, Storage 플러그인 추가
    - google-services.json 및 firebase_options.dart 추가
    - Firebase 관련 초기화 코드(lib/main.dart) 설정
    - Firebase 규칙 및 설정 파일 추가 (firestore.rules, firebase.json 등)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_p/firebase_options.dart';
 import 'screens/login_screen.dart'; // LoginScreen이 있는 파일 경로
 import 'screens/review_screen.dart'; // ReviewScreen이 있는 파일 경로
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
   WidgetsFlutterBinding.ensureInitialized(); // Zone 초기화
   runApp(HansungMarketApp());
 }
