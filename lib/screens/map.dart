36e7eaa6c4068fbed252d01b4960af2104923e86
seojengchan <sjc07020916@gmail.com>
2024-12-06 18:33:03 +0900

    지도 물품 연동

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,6 +1,11 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import './home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 
 class Mapscreen extends StatefulWidget {
   const Mapscreen({Key? key}) : super(key: key);
@@ -11,118 +16,130 @@ class Mapscreen extends StatefulWidget {
 
 class _MapscreenState extends State<Mapscreen> {
   late GoogleMapController _mapController;
-
-  final LatLng _initialPosition = LatLng(37.582557057513185, 127.01022325889619); // 한성대의 위도와 경도
+  final LatLng _initialPosition = LatLng(37.582557057513185, 127.01022325889619);
   final Set<Marker> _markers = {};
-
-  double _currentZoom = 15; // 초기 줌 레벨
+  double _currentZoom = 15;
 
   @override
   void initState() {
     super.initState();
-    // 초기 마커 추가
-    _markers.add(
-      Marker(
-        markerId: MarkerId('initial_marker'),
-        position: _initialPosition,
-        infoWindow: InfoWindow(
-          title: '한성대학교',
-          snippet: '한성대학교',
-        ),
-      ),
-    );
+    _loadMarkers();
   }
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
   }
 
-  // 줌인 기능
+  Future<void> _loadMarkers() async {
+    final items = await FirebaseFirestore.instance
+        .collection('items')
+        .where('location', isNotEqualTo: null)
+        .get();
+
+    for (var doc in items.docs) {
+      final data = doc.data();
+      final location = data['location'] as GeoPoint?;
+      final imageUrl = data['image'] ?? '';
+
+      if (location != null) {
+        final markerIcon = await _getOptimizedMarkerIcon(imageUrl);
+
+        final marker = Marker(
+          markerId: MarkerId(doc.id),
+          position: LatLng(location.latitude, location.longitude),
+          infoWindow: InfoWindow(
+            title: data['title'],
+            snippet: data['description'],
+          ),
+          icon: markerIcon,
+        );
+
+        setState(() {
+          _markers.add(marker);
+        });
+      }
+    }
+  }
+
+  Future<BitmapDescriptor> _getOptimizedMarkerIcon(String imageUrl) async {
+    try {
+      final Uint8List imageData = await _fetchImageData(imageUrl);
+      final Uint8List resizedImage = await _resizeImage(imageData, 100);
+      return BitmapDescriptor.fromBytes(resizedImage);
+    } catch (e) {
+      print("Error loading marker icon: $e");
+      return BitmapDescriptor.defaultMarker;
+    }
+  }
+
+  Future<Uint8List> _fetchImageData(String imageUrl) async {
+    final Uri uri = Uri.parse(imageUrl);
+    final HttpClient httpClient = HttpClient();
+    final HttpClientRequest request = await httpClient.getUrl(uri);
+    final HttpClientResponse response = await request.close();
+    final Completer<Uint8List> completer = Completer<Uint8List>();
+    final List<int> bytes = [];
+    response.listen(
+          (chunk) => bytes.addAll(chunk),
+      onDone: () => completer.complete(Uint8List.fromList(bytes)),
+      onError: (e) => completer.completeError(e),
+    );
+    return completer.future;
+  }
+
+  Future<Uint8List> _resizeImage(Uint8List data, int size) async {
+    final ui.Codec codec = await ui.instantiateImageCodec(data, targetWidth: size, targetHeight: size);
+    final ui.FrameInfo frameInfo = await codec.getNextFrame();
+    final ByteData? byteData = await frameInfo.image.toByteData(format: ui.ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
+  }
+
   void _zoomIn() {
     setState(() {
-      _currentZoom += 1; // 줌 레벨 증가
-      _mapController.animateCamera(
-        CameraUpdate.zoomTo(_currentZoom),
-      );
+      _currentZoom += 1;
+      _mapController.animateCamera(CameraUpdate.zoomTo(_currentZoom));
     });
   }
 
-  // 줌아웃 기능
   void _zoomOut() {
     setState(() {
-      _currentZoom -= 1; // 줌 레벨 감소
-      _mapController.animateCamera(
-        CameraUpdate.zoomTo(_currentZoom),
-      );
+      _currentZoom -= 1;
+      _mapController.animateCamera(CameraUpdate.zoomTo(_currentZoom));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        // 뒤로 가기 버튼을 눌렀을 때 홈 화면으로 이동
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => Home()),
-        );
-        return false; // 현재 화면에서 뒤로 가기 동작 차단
-      },
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('지도'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('지도'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: _currentZoom,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _initialPosition,
-            zoom: _currentZoom, // 초기 줌 레벨
+        markers: _markers,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: _zoomIn,
+            child: Icon(Icons.zoom_in),
           ),
-          markers: _markers,
-          myLocationEnabled: true, // 사용자의 현재 위치 표시
-          myLocationButtonEnabled: true, // 현재 위치 버튼 활성화
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: _zoomIn, // 줌인 버튼
-              child: Icon(Icons.zoom_in),
-            ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: _zoomOut, // 줌아웃 버튼
-              child: Icon(Icons.zoom_out),
-            ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: () {
-                // 새로운 위치 추가
-                setState(() {
-                  LatLng newLocation = LatLng(37.5775, 126.9895); // 다른 위치
-                  _markers.add(
-                    Marker(
-                      markerId: MarkerId('new_marker'),
-                      position: newLocation,
-                      infoWindow: InfoWindow(
-                        title: '새로운 마커',
-                        snippet: '여기에 새로운 위치를 추가합니다.',
-                      ),
-                    ),
-                  );
-                  _mapController.animateCamera(
-                    CameraUpdate.newLatLng(newLocation),
-                  );
-                });
-              },
-              child: Icon(Icons.add_location),
-            ),
-          ],
-        ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: _zoomOut,
+            child: Icon(Icons.zoom_out),
+          ),
+        ],
       ),
     );
   }
